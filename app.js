1fe6adc6cba420ae0279fa77fa723fa13fb5b4c7
akif-onder <akifonder@gmail.com>
2022-11-10 11:53:49 +0300

    css

---
+++ app.js
@@ -0,0 +1,26 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+
+const cellElements = document.querySelectorAll("[data-cell]")
+let circleTurn
+
+cellElements.forEach(cell=>{
+    cell.addEventListener('click', handleClick, {once:true})
+})
+
+function handleClick(e){
+    const cell = e.target
+    
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    //place Mark
+    placeMark(cell,currentClass)
+    //Check for Win
+    //Check for Draw
+    //SwitchTurns
+
+    console.log('clicked')
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.a
+}
